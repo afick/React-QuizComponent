a610ace3abe102a931fb646fd92812b3e11ff25c
Mandy Fick Anderson <mfick@vestmark.com>
2019-05-30 15:29:07 -0700

    fix quiz var

--- src/Quiz.js
+++ src/Quiz.js
@@ -12,7 +12,7 @@ class Quiz extends Component {
     return (
       <div>
         <div className='QuizQuestion'>
-          {quizdata.quiz_questions[0].instruction_text}
+          {quizData.quiz_questions[0].instruction_text}
         </div>
       </div>
     );

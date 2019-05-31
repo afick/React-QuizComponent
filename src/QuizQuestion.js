0c93a6fa8376905d3b539dd8e83b7801da771356
Mandy Fick Anderson <mfick@vestmark.com>
2019-05-30 17:06:27 -0700

    fix issue

--- src/QuizQuestion.js
+++ src/QuizQuestion.js
@@ -8,7 +8,7 @@ class QuizQuestion extends Component {
   }
   handleClick(buttonText) {
     if (buttonText === this.props.quiz_question.answer) {
-      this.props.showNextQuestion();
+      this.props.showNextQuestionHandler();
     }
   }
   render() {
